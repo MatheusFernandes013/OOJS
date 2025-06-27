38b05b20d587536721de4e91d6a8bfcac26267bc
Matheus <matheus.oliveirag@outlook.com.br>
2025-06-27 13:23:35 -0300

    Polimorfismo

--- funcionario.js
+++ funcionario.js
@@ -6,11 +6,47 @@ function Pessoa(nome) {
 }
 function Funcionario(nome, cargo, salario) {
     this.cargo = cargo;
-    this.salario = salario;
+    let _salario = salario;
+    this.getSalario = function() {
+        return _salario;
+    }
+    this.setSalario = function(valor) {
+        if(typeof valor === 'number') {
+            _salario = valor;
+        }else {
+            console.log("Valor inválido para salário.");
+        }
+    }
+    this.aumento = function() {
+        const novoSalario = _salario * 1.1;
 
+        
+    }
+    this.dizCargo = function() {
+        console.log(this.cargo);
+    }
     Pessoa.call(this, nome);
+}
+
+function Estagiario(nome) {
+    Funcionario.call(this, nome, "Estagiário", 2000);
+    this.aumento = function() {
+        const novoSalario = this.getSalario() * 1.07;
+        this.setSalario(novoSalario);
+        console.log(this.getSalario());
+    }
 }
 const pessoa1 = new Pessoa("João");
 const funcionario1 = new Funcionario("Maria", "Desenvolvedora", 5000);
-console.log(pessoa1);
-console.log(funcionario1);
+// console.log(pessoa1);
+// console.log(funcionario1);
+
+// funcionario1.dizOi();
+// funcionario1.dizCargo();
+// funcionario1.setSalario(7000);
+
+funcionario1.aumento();
+console.log(funcionario1.getSalario());
+
+const estagiario1 = new Estagiario("Pedro");
+estagiario1.aumento();
